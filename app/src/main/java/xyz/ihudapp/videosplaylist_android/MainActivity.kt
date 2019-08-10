3cf5c9345c38bcde115932651a068f2a52ad3cb4
israelhudson <israelhudson@gmail.com>
2019-08-10 11:14:27 -0300

    examplo com ExoPlayer simples porem usando funcões depreciadas

--- app/src/main/java/xyz/ihudapp/videosplaylist_android/MainActivity.kt
+++ app/src/main/java/xyz/ihudapp/videosplaylist_android/MainActivity.kt
@@ -1,12 +1,34 @@
 package xyz.ihudapp.videosplaylist_android
 
+import android.content.Context
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var playerView : PlayerView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playerView  = findViewById(R.id.ep_video_view)
+
+
     }
+
+
 }
